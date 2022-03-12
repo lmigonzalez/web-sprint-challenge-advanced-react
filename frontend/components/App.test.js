7a07548eb2a4d9b219ecb5f0239c7a27d2c6f9d2
Luis Gonzalez <luisgonzalez@Luiss-MacBook-Pro.local>
2022-03-11 22:09:57 -0500

    first submit half way!

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,24 @@
 // Write your tests here
+
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import AppFunctional from './AppFunctional'
+
+
+
 test('sanity', () => {
   expect(true).toBe(false)
+})
+
+
+
+test('render on the screen', () =>{
+
+  render(<AppFunctional/>)
+
+  const header = screen.getByText(/Coordinates/i, {exact: false});
+  expect(header).toBeVisible();
+  
 })
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,32 +1,142 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function AppFunctional(props) {
+
+const coordinates = [
+  ['1, 1'], ['2, 1'], ['3, 1'],
+  ['1, 2'], ['2, 2'], ['3, 2'],
+  ['1, 3'], ['2, 3'], ['3, 3']
+];
+
+const grid = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const initialValues = {
+   initialMSG: '',
+   initialPos: grid[4],
+   initialMove: 0,
+   initialCoord: coordinates[4]
+}
+
+
+const [pos, setPos] = useState(initialValues.initialPos);
+const [errorMsg, setErrorMsg] = useState(initialValues.initialMSG);
+const [moved, setMoved] = useState(initialValues.initialMove);
+const [coord, setCoord] = useState(initialValues.initialCoord)
+
+const getCoord = () =>{
+  setCoord(coordinates[pos])
+}
+
+const whenMoved = () =>{
+  setMoved(moved + 1)
+}
+
+const topArrow = () =>{
+  
+  if(pos - 3 <= 0){
+    setPos(pos)
+    setErrorMsg("You can't go up")
+  }
+  else{
+    setPos(pos - 3)
+    setErrorMsg(initialMSG)
+    whenMoved()
+    getCoord()
+    }
+}
+
+const rightArrow = () =>{
+  
+  if(pos + 1 === 4 || pos + 1 === 7 || pos + 1 === 10){
+    setPos(pos)
+    setErrorMsg("You can't go right")
+  }
+  else{
+    setPos(pos + 1)
+    setErrorMsg(initialMSG)
+    whenMoved()
+    getCoord()
+    }
+}
+
+
+const downArrow = () =>{
+  
+  if(pos + 3 > 9){
+    setPos(pos)
+    setErrorMsg("You can't go down")
+  }
+  else{
+    setPos(pos + 3)
+    setErrorMsg(initialMSG)
+    whenMoved()
+    getCoord()
+    }
+}
+ 
+
+const leftArrow = () =>{
+  
+  if(pos - 1 === 0 || pos - 1 === 3 || pos - 1 === 6){
+    setPos(pos)
+    setErrorMsg("You can't go left")
+  }
+  else{
+    setPos(pos - 1)
+    setErrorMsg(initialMSG)
+    whenMoved()
+    getCoord()
+    }
+}
+
+const reset = () =>{
+  setPos(initialPos)
+  setMoved(initialMove)
+  setCoord(initialCoord)
+}
+ 
+
+
+
+
+ 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates ({coord})</h3>
+        <h3 id="steps">You moved {moved} times</h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        {
+          grid.map((element, index)=>{
+          //  console.log(JSON.stringify(element) === JSON.stringify(pos))
+
+          // const newStyle = JSON.stringify(element) === JSON.stringify(pos) ? 'square active': 'square';
+          // setStyle(newStyle)
+         
+            return (
+            <div
+            value={element} 
+            key={index} 
+            className= {element === pos ? 'square active': 'square'}> 
+            
+            {element === pos ? 'B': ''}
+            
+            </div>)
+
+          })
+          
+        }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{errorMsg}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={leftArrow}>LEFT</button>
+        <button id="up" onClick={topArrow}>UP</button>
+        <button id="right" onClick={rightArrow}>RIGHT</button>
+        <button id="down" onClick={downArrow}>DOWN</button>
+        <button id="reset" onClick={reset}>reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
@@ -35,3 +145,14 @@ export default function AppFunctional(props) {
     </div>
   )
 }
+
+
+  {/* <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square active">B</div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div> */}
